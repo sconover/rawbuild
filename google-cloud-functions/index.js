35e9b0aaf762063919dbd8b4e599738c911604f3
Steve Conover <steve.conover@soundcloud.com>
2018-03-15 17:56:23 +0100

    minimal config file example

--- google-cloud-functions/index.js
+++ google-cloud-functions/index.js
@@ -1,9 +1,15 @@
+const path = require('path')
+const fs = require('fs')
+
+const configPath = path.join(__dirname, 'config.json')
+const configContents = fs.readFileSync(configPath, 'utf8')
+
 exports.onHelloHttpTrigger = function(httpRequest, httpResponse) {
   console.log("gcf http", httpRequest.body)
-  httpResponse.send(`hi from gcf http function`)
+  httpResponse.send(`hi from gcf http function ${configContents}`)
 }
 
 exports.onHelloGCSTrigger = function(event, callback) {
-  console.log("onHelloGCSTrigger", event)
+  console.log("onHelloGCSTrigger", event, configContents)
   callback()
 }
